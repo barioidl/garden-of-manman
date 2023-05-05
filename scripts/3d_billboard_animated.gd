5b739089de9dc838b4d3635b9057c6994ab474ad
BarioIDL <75007632+barioidl@users.noreply.github.com>
2023-05-05 18:51:12 +0700

    moving to 3d style

--- scripts/3d_billboard_animated.gd
+++ scripts/3d_billboard_animated.gd
@@ -1,19 +1,17 @@
 extends Node
-@export var sun_face:BillboardSides
-@export var moon_face:BillboardSides
+@export var sun_face:Node3D
+@export var moon_face:Node3D
 
-@onready var face :=$".."
-@onready var lamp := $"../OmniLight3D"
+@onready var parent :=$".."
+@export var lamp :OmniLight3D
 @export var switch_sfx:AudioList
 func _ready() -> void:
 	switch(false)
 func switch(is_sun):
-	var pos = face.global_position
+	var pos = parent.global_position
 	AudioPool.create_sound_3d(pos,switch_sfx)
 	
-	if is_sun:
-		face.set("sprites",sun_face)
-		lamp.light_energy = 1
-	else:
-		face.set("sprites",moon_face)
-		lamp.light_energy = 0.1
+	sun_face.visible = is_sun
+	moon_face.visible = !is_sun
+	
+	lamp.light_energy = 1 if is_sun else 0.1
